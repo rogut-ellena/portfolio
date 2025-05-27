824a5079b988db4e4ce89401bce56a9eba4716d3
unknown <rogut.ellena@gmail.com>
2025-05-27 22:42:24 +0300

    update the site/url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,7 +17,7 @@ export default defineNuxtConfig({
 
   devtools: { enabled: true },
    site: { 
-    url: 'https://example.com', 
+    url: 'https://portfolio-lac-sigma-75.vercel.app/', 
     name: 'My Awesome Website' 
     }, 
   app: {
